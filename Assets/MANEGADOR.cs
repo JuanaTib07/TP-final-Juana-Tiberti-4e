05a17e65b0724932f01615181e9dc21fefdc88da
JuanaTib07 <juanitatiberti@gmail.com>
2024-06-19 15:21:21 -0300

    comida

---
+++ Assets/MANEGADOR.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MANEGADOR : MonoBehaviour
+{
+    public GameObject[] comida;
+    public int currentIndex = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DesactivateAll(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentIndex++;
+
+            if (currentIndex < comida.Length)
+            {
+                DesactivateAll();
+                ActivateIndex(currentIndex);
+            }
+
+            else if (currentIndex >= comida.Length)
+            {
+                currentIndex = -1;
+            }
+            
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            currentIndex--;
+
+            if (currentIndex < comida.Length)
+            {
+                DesactivateAll();
+                ActivateIndex(currentIndex);
+            }
+
+            else if (currentIndex >= comida.Length)
+            {
+                currentIndex = 13;
+            }
+
+        }
+    }
+
+    void DesactivateAll()
+    {
+        for (int i = 0; i < comida.Length; i++)
+        {
+            comida[i].SetActive(false);
+        }
+    }
+
+    void ActivateIndex(int index)
+    {
+        comida[index].SetActive(true);
+    }
+
+
+
+}
